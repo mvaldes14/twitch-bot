0ea10efaa89bc661456770943efbbc164df55454
mvaldes <mvaldes.revilla@gmail.com>
2024-01-14 22:54:02 -0600

    adding github action

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Go Runtime for Project";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = { nixpkgs, self }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system;};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go
+        ];
+      };
+    };
+}
+
