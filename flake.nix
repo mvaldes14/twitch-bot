59d6aeb0ff1d7c2f0aada42a6d8c98ed01a495da
mvaldes <mvaldes.revilla@gmail.com>
2024-02-07 18:58:08 -0600

    testing building things with nix

--- flake.nix
+++ flake.nix
@@ -5,15 +5,34 @@
   outputs = { nixpkgs, self }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system;};
+      pkgs = import nixpkgs { inherit system; };
+      name = "twitch-bot";
+      username = "mvaldes14";
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
+        buildInputs = with pkgs;[
           go
           act
         ];
       };
+      packages = {
+        default = pkgs.buildGoPackage {
+          inherit name;
+          goPackagePath = "github.com/${username}/${name}";
+          goDeps = [
+            {
+              goPackagePath = "github.com/gempir/go-twitch-irc";
+              fetch = {
+                type = "git";
+                url = "https://github.com/gempir/go-twitch-irc";
+                rev = "01bg6bx8ivqww56m2s73yi0991n18bsp366hd3vxic62ax7q4qy5";
+                hash = "something";
+              };
+            }
+          ];
+          src = ./.;
+        };
+      };
     };
 }
-
